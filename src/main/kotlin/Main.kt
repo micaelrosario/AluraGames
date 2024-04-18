ba9d52f7efd306ec180346e0029e60f1d4c7e3dd
Micael Rosário <micaeldevrosario@gmail.com>
2024-04-18 19:53:28 -0300

    Lendo id do jogo a ser buscado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,18 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
 import com.google.gson.Gson
-
+import java.util.*
 
 fun main() {
+    val leitura = Scanner(System.`in`)
+        println("Digite um código de jogo para buscar: ")
+    val busca = leitura.nextLine()
+
+    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$busca"
+
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=612"))
+        .uri(URI.create(endereco))
         .build()
     val response = client
         .send(request, BodyHandlers.ofString())

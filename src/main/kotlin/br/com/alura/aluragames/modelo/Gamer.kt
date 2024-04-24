0a9c052f31559e8d77f4a992f5bb33b7209303fe
Micael Rosário <micaeldevrosario@gmail.com>
2024-04-24 06:49:47 -0300

    Criando classe gamer e seus comportamentos

---
+++ src/main/kotlin/br/com/alura/aluragames/modelo/Gamer.kt
@@ -0,0 +1,24 @@
+package br.com.alura.aluragames.modelo
+
+data class Gamer(val nome:String, val email:String){
+    var dataNascimento:String? = null
+    var usuario:String? = null
+    var idInterno:String? = null
+
+    constructor(nome:String, email:String, dataNascimento: String, usuario: String): this(nome, email){
+        this.dataNascimento = dataNascimento
+        this.usuario = usuario
+    }
+
+    override fun toString(): String {
+        return "Gamer:\n" +
+                "Nome: $nome\n" +
+                "Email: $email\n" +
+                "Data de Nascimento: $dataNascimento\n" +
+                "Usuário: $usuario\n" +
+                "ID Interno: $idInterno"
+    }
+
+
+
+}
